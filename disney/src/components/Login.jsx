512c62a69d315702bd6a5cb31fd67e4619dec3db
Ayush <unitedestates.ayush10@gmail.com>
2024-03-17 17:56:37 +0530

    vite config

---
+++ disney/src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+const Login = (props) => {
+  return (
+    <div>
+      <div>Logadgin</div>;<div>Logadgin</div>;<div>Logadggkin</div>;
+      <div>Logadgin</div>;
+    </div>
+  );
+};
+
+export default Login;
